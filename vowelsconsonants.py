73f9cc2b89f5eb9a18136e819d3facac0015c993
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-01-07 15:44:34 +0530

    Create vowelsconsonants.py

---
+++ vowelsconsonants.py
@@ -0,0 +1,17 @@
+s=input()
+if s.isalpha():
+  l=['a','e','i','o','A','E','I','O','U']
+  if s in l:
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")s=input()
+if s.isalpha():
+  l=['a','e','i','o','A','E','I','O','U']
+  if s in l:
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")
