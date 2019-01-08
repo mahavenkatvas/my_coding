9e5e53a088cb515909846b2b78062dcc6944c9d6
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-01-08 11:29:21 +0000

    Create amsrange.py

---
+++ amsrange.py
@@ -0,0 +1,15 @@
+n,k=map(int,input().split(" "))
+l=[]
+for n in range(n+1,k):
+    x=n
+    ams=0
+    while n>0:
+        r=n%10
+        ams=ams+r**3
+        n=n//10
+    if x==ams:
+        l.append(x)
+print(l)
+for i in range(len(l)-1):
+  print(l[i],end=" ")
+print(l[-1])  
