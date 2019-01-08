3cb41f5ff0f3c6ca1be88b8d80efbd2d344f00eb
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-01-08 10:32:14 +0000

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,6 +1,13 @@
+#mahalakshmi
 n,k=map(int,input().split(" "))
 n=n+1
-while n<=k:
+l=[]
+while n<k:
   if n%2==1:
-    print(n,end=" ")
-  n+=1  
+    l.append(n)
+  n+=1 
+for i in range(len(l)-1):
+  print(l[i],end=" ")
+print(l[-1])
+
+  
