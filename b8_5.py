860cbde962accbcdb54876f7b2845dd904bccac7
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-02-12 15:39:39 +0530

    Update b8_5.py

--- b8_5.py
+++ b8_5.py
@@ -2,9 +2,7 @@ s=input()
 r=""
 x=len(s)//2
 if len(s)%2==0:
-	for i in range(len(s)):
-		print(s[:x-1]+"**"+s[x+1:])
+	print(s[:x-1]+"**"+s[x+1:])
 else:
-	for i in range(len(s)):
-		print(s[:x]+"*"+s[x+1:])
+	print(s[:x]+"*"+s[x+1:])
 		
