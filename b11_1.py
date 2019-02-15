e99d7875b32178e09482b47e07535ed9bad68d6c
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-02-15 16:08:02 +0530

    Update b11_1.py

--- b11_1.py
+++ b11_1.py
@@ -1,8 +1,10 @@
 s,n=map(str,input().split(" "))
 n=int(n)
 x=s[::-1]
+l=[]
 for i in range(n):
-	print(x[i],end="")
-
-	
+	l.append(x[i])
+l.reverse()
+for i in range(len(l)):
+	print(l[i],end="")
 	
