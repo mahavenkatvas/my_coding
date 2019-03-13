0b414d58095c751ba1fa6bf7f3fd9803792d0a1d
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-03-13 19:04:41 +0530

    Create p1_5.py

---
+++ p1_5.py
@@ -0,0 +1,6 @@
+#maha
+s=input()
+l=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(len(l)):
+      if s == l[i]:
+          print(i+1)
