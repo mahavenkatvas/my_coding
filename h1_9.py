ea63443503f00e95861743e7fb24955b085bbc9f
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-05-13 13:53:15 +0530

    Update h1_9.py

--- h1_9.py
+++ h1_9.py
@@ -3,8 +3,9 @@ l=[int(x) for x in input().split()]
 for i in range(len(l)):
     if l[i]<0:
         for j in range(len(l)):
-            if abs(l[i])==l[j]:
+            if abs(l[i])<=l[j]:
                 print(l[i],l[j])
                 break
-if l.count(0)%2==0:
+s=l.count(0)
+if s>1 and s%2==0:
     print("0 0")
