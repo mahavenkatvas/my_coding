e981a3d250552f635137ac6b86f6260f53b23161
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-05-13 07:15:51 +0530

    Update h1_6.py

--- h1_6.py
+++ h1_6.py
@@ -1,6 +1,15 @@
 n=int(input())
 l=[int(x) for x in input().split()]
+k=[]
 for i in range(len(l)):
+     x=l.count(l[i])
+     k.append(x)
+h=k.count(1)
+if h==0:
+   print("unique")
+else:
+   for i in range(len(l)):
      if l.count(l[i])>1:
-         print(l[i])
-         break
+        print(l[i])
+        break
+      
