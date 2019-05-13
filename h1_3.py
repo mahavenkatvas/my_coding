c9b43d7f74cee88b7aa4acbf14aa8c5d1cb06d54
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-05-13 07:00:38 +0530

    Update h1_3.py

--- h1_3.py
+++ h1_3.py
@@ -4,7 +4,9 @@ x=[]
 for i in range(len(l)):
      if l[i]==i:
         x.append(i)
-
-for i in range(len(x)-1):
-      print(x[i],end=" ")
-print(x[-1])
+if len(x)==0:
+     print("-1")
+else:
+     for i in range(len(x)-1):
+          print(x[i],end=" ")
+     print(x[-1])
