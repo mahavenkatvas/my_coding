3d44083583458eb46dfba62f71e275b4292de4d6
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-05-14 11:48:19 +0530

    Update p3_5.py

--- p3_5.py
+++ p3_5.py
@@ -1,8 +1,7 @@
+n=int(input())
 l=list(map(str,input().split()))
 p=sorted(l,key=len)
-n=int(input())
-print(p)
 for i in range(len(p)-1):
-    if len(p[i])==len(p[i+1]) and p[i]>p[i+1]:
-    	p[i],p[i+1]=p[i+1],p[i]
+	if len(p[i])==len(p[i+1]) and p[i]>p[i+1]:
+		p[i],p[i+1]=p[i+1],p[i]
 print(*p)
