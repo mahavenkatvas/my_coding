d347655cb5729fbd3694ab679eca66e2e84fafb2
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-05-14 10:32:15 +0530

    Update p3_1.py

--- p3_1.py
+++ p3_1.py
@@ -1,6 +1,6 @@
-a,b=map(int,input().split(" "))
-c,d=map(int,input().split(" "))
-e,f=map(int,input().split(" "))
+a,b=map(int,input().split())
+c,d=map(int,input().split())
+e,f=map(int,input().split())
 if a==c and c==e:
 	print("yes")
 elif b==d and d==f:
