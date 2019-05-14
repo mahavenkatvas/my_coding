bacf5edbcc158f39443b61eaf31a720effdaec31
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-05-14 10:17:31 +0530

    Create p3_1.py

---
+++ p3_1.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split(" "))
+c,d=map(int,input().split(" "))
+e,f=map(int,input().split(" "))
+if a==c and c==e:
+	print("yes")
+elif b==d and d==f:
+	print("yes")
+else:
+	print("no")
