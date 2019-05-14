e8c64bd2b86e85e3b6008df208efa72c8dcfbd8e
mahavenkatvas <46440663+mahavenkatvas@users.noreply.github.com>
2019-05-14 10:46:52 +0530

    Create p3_3.py

---
+++ p3_3.py
@@ -0,0 +1,9 @@
+n,k=map(int,input().split())
+l=[int(x) for x in input().split()]
+l1=[int(x) for x in input().split()]
+s=[]
+for i in range(len(l1)):
+	l.append(l1[i])
+	x=max(l)
+	s.append(x)
+print(*s)
